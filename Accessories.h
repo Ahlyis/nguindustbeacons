3361fe75396398f0555abc6ff187da9ad898fde1
Ahlyis <82900406+Ahlyis@users.noreply.github.com>
2021-04-22 00:29:05 -0700

    Update Accessories.h

--- Accessories.h
+++ Accessories.h
@@ -13,7 +13,7 @@
 #define MAX_WALL 12
 #define MAX_DONUT 16
 
-bool SetWhichToCheck (int maps);
+void SetWhichToCheck (int maps);
 int  MakeNumber (char *sData);
 bool LoadOperatingMode (int *maps, int *options, int *beacons, int *beacontype);
 int  CalculateBestBeacons (int options, int beacons, int beacontype);
@@ -25,3 +25,5 @@ void CalcArValues (int *arrhitby, int *arravail, int *arlhitby, int *arlavail,
                    int *ardhitby, int *ardavail, int *aruhitby, int *aruavail, int x, int y);
 void CalcWlValues (int *hwlhitby, int *hwlavail, int *vwlhitby, int *vwlavail, int x, int y);
 void CalcDtValues (int *dthitby, int *dtavail, int x, int y);
+void Shuffle ();
+void CopyMap (int sourcemap[MAX_ROW][MAX_COL], int destmap[MAX_ROW][MAX_COL]);
